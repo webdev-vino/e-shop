8086b3df72b12ffccec08b4a66db3d34cce2cd64
DevelopeWeb <vengadeshsugu21@gmail.com>
2023-12-25 18:16:52 +0530

    updated

--- api/server.js
+++ api/server.js
@@ -46,7 +46,7 @@ app.post('/email', async (req, res) => {
               res.json(err);
           }
 })
-mongoose.connect("mongodb://0.0.0.0:27017/appDB")
+mongoose.connect("mongodb://127.0.0.1:27017/appDB")
     .then(() => {
         app.listen(port, () => {
             console.log(`server is running on port: ${port}`);
